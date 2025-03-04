a0996236702d57f4aebfb1fa999facf6e0f8c854
FILIPRAIDER <filipraider123@gmail.com>
2025-03-03 21:28:44 -0500

    Correccion

--- src/data/publicaciones.js
+++ src/data/publicaciones.js
@@ -6,7 +6,7 @@ export const publicaciones = [
         descripcion: "El Instituto de Estudios en Educación y el Centro de Consultoría de la universidad brindan apoyo a la Secretaría de Educación Departamental en la estructuración del Plan Decenal de Educación 2024-2034. El 27 de noviembre compartieron con diputados del Atlántico los avances y recibieron retroalimentación.",
         tipoPublicacion: "Tipo de publicación",
         slug: "titulo-1",
-        imagen: "/src/assets/1.jpg"
+        imagen: "/assets/1.jpg"
     },
     {
         titulo: "Córdoba fortalece la educación con nuevas estrategias de formación docente",
@@ -14,7 +14,7 @@ export const publicaciones = [
         descripcion: "El Instituto de Estudios en Educación y el Centro de Consultoría de la universidad brindan apoyo a la Secretaría de Educación Departamental en la estructuración del Plan Decenal de Educación 2024-2034. El 27 de noviembre compartieron con diputados del Atlántico los avances y recibieron retroalimentación.",
         tipoPublicacion: "Tipo de publicación",
         slug: "titulo-2",
-        imagen: "/src/assets/2.jpg"
+        imagen: "/assets/2.jpg"
     },
     {
         titulo: "Título",
@@ -22,7 +22,7 @@ export const publicaciones = [
         descripcion: "El Instituto de Estudios en Educación y el Centro de Consultoría de la universidad brindan apoyo a la Secretaría de Educación Departamental en la estructuración del Plan Decenal de Educación 2024-2034. El 27 de noviembre compartieron con diputados del Atlántico los avances y recibieron retroalimentación.",
         tipoPublicacion: "Tipo de publicación",
         slug: "titulo-3",
-        imagen: "/src/assets/3.jpg"
+        imagen: "/assets/3.jpg"
     },
     {
         titulo: "Título",
@@ -30,7 +30,7 @@ export const publicaciones = [
         descripcion: "El Instituto de Estudios en Educación y el Centro de Consultoría de la universidad brindan apoyo a la Secretaría de Educación Departamental en la estructuración del Plan Decenal de Educación 2024-2034. El 27 de noviembre compartieron con diputados del Atlántico los avances y recibieron retroalimentación.",
         tipoPublicacion: "Tipo de publicación",
         slug: "titulo-4",
-        imagen: "/src/assets/4.jpg"
+        imagen: "/assets/4.jpg"
     },
     {
         titulo: "Título",
@@ -38,6 +38,6 @@ export const publicaciones = [
         descripcion: "El Instituto de Estudios en Educación y el Centro de Consultoría de la universidad brindan apoyo a la Secretaría de Educación Departamental en la estructuración del Plan Decenal de Educación 2024-2034. El 27 de noviembre compartieron con diputados del Atlántico los avances y recibieron retroalimentación.",
         tipoPublicacion: "Tipo de publicación",
         slug: "titulo-5",
-        imagen: "/src/assets/5.jpg"
+        imagen: "/assets/5.jpg"
     }
 ];

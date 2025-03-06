4fa57ec36613d16826d90c5dd7ccfccc493e5ade
kevinmorelo13 <kmorelo89@gmail.com>
2025-03-06 13:49:56 -0500

    pagina de contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx'; // 💡 Agrega esta línea
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx"; // 💡 Agrega esta línea
 
 export default defineConfig({
   site: "https://observatorio-educacion.vercel.app",
   integrations: [
+    react(),
     tailwind({
       // Opciones de configuración explícitas
-      config: { path: './tailwind.config.cjs' }
+      config: { path: "./tailwind.config.cjs" },
     }),
-    mdx() // 💡 Ahora mdx() está correctamente importado
-  ]
+    mdx(), // 💡 Ahora mdx() está correctamente importado
+  ],
 });
---
+++ src/components/mapGoogle.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia2V2aW5tb3JlbG8xMyIsImEiOiJjbTd4a2x1MGwwMDM4MmlwdHMwZTJzZmE1In0.asz8yGqkmezHaPIP709Yyg"; // 🔥 Reemplaza con tu token de Mapbox
+
+const Mapa = () => {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    if (!mapContainer.current) return; // Evitar errores en SSR (Server-Side Rendering)
+
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-75.8688645, 8.7662868], // Coordenadas (longitud, latitud)
+      zoom: 15,
+    });
+
+    new mapboxgl.Marker().setLngLat([-75.86886451, 8.7662868]).addTo(map);
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div
+      ref={mapContainer}
+      style={{ width: "100%", height: "100%", borderRadius: "4px" }}
+    />
+  );
+};
+
+export default Mapa;
